b38e1584e331a1c9c076f7bc729c7353c2ae2e4e
baluguru <smkumarag@gmail.com>
2020-11-04 11:58:39 +0530

    added monthwage of employee

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,12 +3,16 @@ public class EmpWageComputation
   public static final int IS_PART_TIME = 1;
   public static final int IS_FULL_TIME = 2;
   public static final int empWagePerHr = 20;
+  public static final int NUM_OF_WORKING_DAYS=20; 
   public static void main(String args[])
   {
       int workedHrs = 0;
       int empWage = 0;
-      int type_of_Employee = (int) Math.floor(Math.random() * 10) % 3;
-      switch (type_of_Employee) 
+      int totalWage=0;
+      for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+      {
+        int type_of_Employee = (int) Math.floor(Math.random() * 10) % 3;
+        switch (type_of_Employee) 
            {
 		case IS_FULL_TIME : 
 		       workedHrs = 8;
@@ -20,7 +24,10 @@ public class EmpWageComputation
 			workedHrs = 0;
 	   }
      empWage = workedHrs * empWagePerHr;
+     totalWage+=empWage;
      System.out.println("Employee Wage : " + empWage);
+     }
+  System.out.println("Total Emp Wage:" +totalWage);
   }
 }
 
