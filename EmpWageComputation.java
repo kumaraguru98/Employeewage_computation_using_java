38d59e4ccfd7b8e90b4adf98a3c6dfee3da16c29
baluguru <smkumarag@gmail.com>
2020-11-04 12:12:28 +0530

    refactored code to compute employeewage for multiple companies

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,25 +2,13 @@ class EmpWageComputation
 {
   public static final int IS_PART_TIME = 1;
   public static final int IS_FULL_TIME = 2;
-  public static final int empWagePerHr = 20;
-  public static final int NUM_OF_WORKING_DAYS=20;
-  public static final int MAX_HOURS_IN_MONTH=100; 
+  public static int computeEmpWage(String company,int empWagePerHr,int numOfWorkingDays,int MAX_HOURS_IN_MONTH) 
+  {
   int workedHrs = 0;
-  int empWage=0;
-  int totalWage=0;
   int totalWorkingHours = 0;
   int totalWorkingDays=0;
 
-  public void wageComputation()
-  {
-     empWage=workedHrs*empWagePerHr;
-     totalWage+=empWage;
-     totalWorkingHours+=workedHrs;
-     System.out.println("Employee's day " + totalWorkingDays + " Wage is: " +empWage);
-  }
-  public void computation()
-  {
-     while(totalWorkingDays<NUM_OF_WORKING_DAYS && totalWorkingHours<MAX_HOURS_IN_MONTH)
+  while(totalWorkingHours<=MAX_HOURS_IN_MONTH && totalWorkingDays<numOfWorkingDays)
      {
         totalWorkingDays++;
         int type_of_Employee = (int) Math.floor(Math.random() * 10) % 3;
@@ -38,15 +26,17 @@ class EmpWageComputation
                       System.out.println("Employee is absent");
 		      workedHrs = 0;
 	   }
-        wageComputation();
-      }   
-     System.out.println("Total working hours in month:"+totalWorkingHours);
-     System.out.println("Total monthly wages is:"+totalWage);
-  }
-     
-  public static void main(String args[]){
-        EmpWageComputation object=new EmpWageComputation();
-        object.computation();
+           totalWorkingHours+=workedHrs;
+           System.out.println("Day: " + totalWorkingDays + " Emp Hr: " +workedHrs);
+          }
+         int totalWage=totalWorkingHours*empWagePerHr;
+         System.out.println("Total employee wage for company: " +company+" is: "+totalWage);
+         return totalWage;
+       }
+       public static void main(String args[]){
+        computeEmpWage("DMart",20,2,10);
+        computeEmpWage("Reliance",10,4,20);
+      
     }
  }
 
