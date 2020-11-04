acc8771fd033b24e8812e30f08f8ba5f372c1c9a
baluguru <smkumarag@gmail.com>
2020-11-04 11:52:45 +0530

    added solution using switch case statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,28 +1,26 @@
 public class EmpWageComputation 
 {
-   public static void main(String[] args){
-   int IS_FULL_TIME = 1;
-   int IS_PART_TIME = 2;
-	int workedHrs = 0;
-	int empWage = 0;
-	int empWagePerHr = 20;
-
-	double type_of_Employee = Math.floor(Math.random() * 10) % 3;
-	if (type_of_Employee == IS_FULL_TIME)
-        {
-	     workedHrs = 8;
-        }
-        else if(type_of_Employee == IS_PART_TIME)
-        {
-             workedHrs = 4;
-        }
-	else
-        {
-	     workedHrs = 0;
-        }
-
-	empWage = workedHrs * empWagePerHr;
-	System.out.println("Employee Wage : " + empWage);
-   }
+  public static final int IS_PART_TIME = 1;
+  public static final int IS_FULL_TIME = 2;
+  public static final int empWagePerHr = 20;
+  public static void main(String args[])
+  {
+      int workedHrs = 0;
+      int empWage = 0;
+      int type_of_Employee = (int) Math.floor(Math.random() * 10) % 3;
+      switch (type_of_Employee) 
+           {
+		case IS_FULL_TIME : 
+		       workedHrs = 8;
+			break;
+		case IS_PART_TIME :
+			workedHrs = 4;
+			break;
+		default :
+			workedHrs = 0;
+	   }
+     empWage = workedHrs * empWagePerHr;
+     System.out.println("Employee Wage : " + empWage);
+  }
 }
 
