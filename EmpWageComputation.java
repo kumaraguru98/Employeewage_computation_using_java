241b3de2510d136a89bfc1a3d0ecb2ca31849a04
baluguru <smkumarag@gmail.com>
2020-11-04 12:03:55 +0530

    added employeewages for a month 20days or 100hours

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,14 +3,16 @@ public class EmpWageComputation
   public static final int IS_PART_TIME = 1;
   public static final int IS_FULL_TIME = 2;
   public static final int empWagePerHr = 20;
-  public static final int NUM_OF_WORKING_DAYS=20; 
+  public static final int NUM_OF_WORKING_DAYS=20;
+  public static final int MAX_HRS_IN_MONTH=100; 
   public static void main(String args[])
   {
       int workedHrs = 0;
-      int empWage = 0;
-      int totalWage=0;
-      for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+      int totalEmpHrs = 0;
+      int totalWorkingDays=0;
+      while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS)
       {
+        totalWorkingDays++;
         int type_of_Employee = (int) Math.floor(Math.random() * 10) % 3;
         switch (type_of_Employee) 
            {
@@ -23,11 +25,11 @@ public class EmpWageComputation
 		default :
 			workedHrs = 0;
 	   }
-     empWage = workedHrs * empWagePerHr;
-     totalWage+=empWage;
-     System.out.println("Employee Wage : " + empWage);
+     totalEmpHrs+=workedHrs;
+     System.out.println("Day: " + totalWorkingDays+ "   Employeee Hours worked:" +workedHrs);
      }
-  System.out.println("Total Emp Wage:" +totalWage);
+     int totalWage=totalEmpHrs*empWagePerHr;
+     System.out.println("Total Emp Wage:" +totalWage);
   }
 }
 
